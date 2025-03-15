506f5731109e3a6b5c4a18d2c6f733a5022b32e0
Dave Horner <dave@midnight.local>
2025-03-15 08:16:32 -0400

    chore: pull build_readme out.

--- cargo-e/build.rs
+++ cargo-e/build.rs
@@ -1,10 +1,8 @@
 // build.rs
 
 mod build_docs;
-mod build_readme;
 
 fn main() {
-    build_readme::update_readme();
     // --- Documentation Copying ---
     // Call our documentation helper to copy media files.
     build_docs::copy_doc_media();
